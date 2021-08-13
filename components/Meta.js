192d5b395b6ebf8668555a38e48a20a068edbd81
hemhem <takutookamoto0427@gmail.com>
2021-08-13 17:26:13 -0400

    update meta tag

--- components/Meta.js
+++ components/Meta.js
@@ -3,10 +3,17 @@ import Head from 'next/head';
 const Meta = ({ title, keywords, description }) => {
   return (
     <Head>
+      <meta charSet='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name="author" content="Takuto Okamoto" />
       <meta name='keywords' content={keywords} />
       <meta name='description' content={description} />
-      <meta charSet='utf-8' />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Latest News App" />
+      <meta property="og:description" content="The latest articles, including technology, business, health, and sports topics, can be seen in the app." />
+      <meta property="og:url" content="https://latest-news-app.vercel.app/search" />
+      <meta property="og:site_name" content="Latest News App" />
+      <meta property="og:locale" content="en_CA" />
       <link rel='icon' href='/favicon-32x32.png' />
       <title>{title}</title>
     </Head>
@@ -15,8 +22,8 @@ const Meta = ({ title, keywords, description }) => {
 
 Meta.defaultProps = {
   title: 'Latest News App',
-  keywords: 'news, latest news, breaking news',
-  description: 'Get the latest news'
+  keywords: 'news, latest news, breaking news, articles, technology, business, health, sports',
+  description: 'The latest articles, including technology, business, health, and sports topics, can be seen in the app'
 }
 
 export default Meta;
